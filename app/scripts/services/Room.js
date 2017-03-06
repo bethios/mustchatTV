46a25ff64c15076d933547eb7d96c49562dc121a
Beth Cummings <bethios@gmail.com>
2017-03-06 13:53:12 -0600

    show messages for current room

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,12 +1,17 @@
 (function() {
     function Room($firebaseArray) {
-        var ref = firebase.database().ref().child("rooms");
-        var rooms = $firebaseArray(ref);
+        var roomRef = firebase.database().ref().child("rooms");
+        var rooms = $firebaseArray(roomRef);
+
+        var messageRef = firebase.database().ref().child("messages");
 
         return {
             all: rooms,
             create: function(roomName){
                 rooms.$add({ name: roomName });
+            },
+            getMessagesById: function(activeRoomId){
+                return $firebaseArray(messageRef.orderByChild("roomId").equalTo(activeRoomId));
             }
         };
 }
--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,16 @@
 (function() {
     function HomeCtrl(Room) {
-        this.heroTitle = "Must Chat TV!";
         this.chatRooms = Room.all;
         this.create = function(newRoomName) {
             Room.create(newRoomName);
-        }
+        };
+        this.activeRoom= null;
+        this.activeRoomMessages = null;
+
+        this.selectedRoom = function(room){
+            this.activeRoom = room;
+            this.activeRoomMessages = Room.getMessagesById(this.activeRoom.$id);
+        };
 
     }
 
